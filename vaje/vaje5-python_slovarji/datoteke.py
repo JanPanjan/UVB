9c787a589948598527aa6b1c57e799b50d2b5eaa
JanPanjan <89231282@student.upr.si>
2025-04-03 09:53:31 +0200

    vaje 6

--- vaje/vaje5-python_slovarji/datoteke.py
+++ vaje/vaje5-python_slovarji/datoteke.py
@@ -1,5 +1,6 @@
 import os
 
+
 files = {}
 
 print(os.listdir("V5-datoteke"))
@@ -12,5 +13,11 @@ for fname in os.listdir(fpath):
         print(f' Reading "{cfn}"...')
         files[fname] = f.readlines()
 
-for data in files:
-    [print(f"{some[0]}:\n {some[1][0].strip()}\n") for some in files.items()]
+for i in range(len(files)-1):
+    k = list(files.keys())
+    v = list(files.values())
+    vv = v[i][0]
+    print(f"{k[i]}: {vv}")
+
+# for data in files:
+#     [print(f"{some[0]}:\n {some[1][0].strip()}\n") for some in files.items()]
